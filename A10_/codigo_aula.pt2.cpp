24649732913d92e87064f0107de4f6174d04218f
Victor Rech Vendruscolo <rechvvictor@gmail.com>
2025-05-22 13:54:29 -0400

    teste

--- A10_/codigo_aula.pt2.cpp
+++ A10_/codigo_aula.pt2.cpp
@@ -5,7 +5,8 @@ using namespace std;
 class DFrac{  //manteremos sempre a fração reduzida
     int num;
     unsigned den;
-unsigned mmc(DFrac other) const{
+
+        unsigned mmc(DFrac other) const{
             unsigned n1,n2,div,mmc;
 
             n1  = den;
@@ -13,41 +14,44 @@ unsigned mmc(DFrac other) const{
             mmc = 1;
             div = 2;
 
-            //enquanto n1>1 ou n2>1, faça            
-            //  se n1 ou n2 é divisivel por div, então
-            //      se n1 é divisivel por div, então
-            //          n1 = n1/div
-            //      se n2 é divisivel por div, então
-            //          n2 = n2/div 
-            //      mmc = mmc * div 
-            //  senão
-            //      div = div + 1 
-            //retorne mmc
-
-            //enquanto n1>1 ou n2>1, faça
             while(n1>1 || n2>1){
-            //  se n1 ou n2 é divisivel por div, então
                 if(!(n1 % div) || !(n2 % div)){
-            //      se n1 é divisivel por div, então
                     if(!(n1 % div))
-            //          n1 = n1/div
                         n1 = n1/div;
-            //      se n2 é divisivel por div, então
                     if(!(n2 % div))
-            //          n2 = n2/div 
                         n2 = n2/div;
-                                        
-            //      mmc = mmc * div 
                     mmc = mmc * div;
                 }
-            //  senão
                 else
-            //      div = div + 1 
                     div++;
             }
-            //retorne mmc
             return mmc;
         }
+
+        unsigned mdc(DFrac other) const{
+            unsigned n1,n2,div,mdc;
+
+            n1  = den;
+            n2  = other.den;
+            mdc = 1;
+            div = 2;
+
+            while(n1>1 || n2>1){
+                if(!(n1 % div) || !(n2 % div)){
+                    if(!(n1 % div)&&!(n2 % div))
+                        mdc = mdc * div;
+                    if(!(n1 % div))
+                        n1 = n1/div;
+                    if(!(n2 % div))
+                        n2 = n2/div;
+                }
+                else
+                    div++;
+            }
+            return mdc;
+        }
+
+
     public:
         DFrac(int num=0,unsigned den=1):
             num(num),
